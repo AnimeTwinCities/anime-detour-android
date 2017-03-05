6595c59f275b92b9d5b23b1b96faec5692a8a9ff
Renee Vandervelde <renee@reneevandervelde.com>
2017-03-05 16:35:15 -0600

    Add Navigation Drawer.

--- app/src/main/java/org/animetwincities/animedetour/MainActivity.java
+++ app/src/main/java/org/animetwincities/animedetour/MainActivity.java
@@ -1,13 +1,23 @@
 package org.animetwincities.animedetour;
 
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import butterknife.BindView;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import inkapplicaitons.android.logger.Logger;
 import inkapplications.android.layoutinjector.Layout;
 import org.animetwincities.animedetour.framework.AppConfig;
 import org.animetwincities.animedetour.framework.BaseActivity;
-import org.animetwincities.animedetour.framework.auth.User;
 import org.animetwincities.animedetour.framework.dependencyinjection.ActivityComponent;
 
 import javax.inject.Inject;
@@ -24,28 +34,120 @@ public class MainActivity extends BaseActivity
     @BindView(R.id.main_headline)
     TextView headline;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.initializeNavigation();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
         this.demoImage.setImageURI("https://placeimg.com/200/200/animals#.jpg");
     }
 
     @Override
-    protected void onNewConfig(AppConfig config) {
+    protected void onNewConfig(AppConfig config)
+    {
         super.onNewConfig(config);
 
         this.headline.setText(config.getTestHeadline());
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
         this.logger.debug("Hello Logging!");
     }
 
     @Override
-    public void injectSelf(ActivityComponent component) {
+    public void injectSelf(ActivityComponent component)
+    {
         component.inject(this);
+    }
+
+    /** Show the full app schedule. */
+    private boolean showSchedule(View view, int i, IDrawerItem iDrawerItem)
+    {
+        this.logger.trace("Showing Schedule");
+        return false;
+    }
+
+    /** Show the "What's New" page. */
+    private boolean showNew(View view, int i, IDrawerItem iDrawerItem)
+    {
+        this.logger.trace("Showing What's New");
+        return false;
+    }
+
+    /** Show the Guests List. */
+    private boolean showGuests(View view, int i, IDrawerItem iDrawerItem)
+    {
+        this.logger.trace("Showing Guest List");
+        return false;
+    }
+
+    /** Show the User Favorites. */
+    private boolean showFavorites(View view, int i, IDrawerItem iDrawerItem)
+    {
+        this.logger.trace("Showing User Favorites");
+        return false;
+    }
+
+    /** Show the Con Map. */
+    private boolean showMap(View view, int i, IDrawerItem iDrawerItem)
+    {
+        this.logger.trace("Showing Map");
+        return false;
+    }
+
+    /** Show the Settings Page. */
+    private boolean showSettings(View view, int i, IDrawerItem iDrawerItem)
+    {
+        this.logger.trace("Showing Settings Page");
+        return false;
+    }
+
+    /**
+     * Set-up the Toolbar and the Navigation Drawer
+     */
+    private void initializeNavigation()
+    {
+        this.setSupportActionBar(toolbar);
+
+        new DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(toolbar)
+            .addDrawerItems(
+                createNavigationItem(R.string.main_menu_new, R.drawable.ic_home_black_24dp, this::showNew),
+                createNavigationItem(R.string.main_menu_guests, R.drawable.ic_people_black_24dp, this::showGuests),
+                createNavigationItem(R.string.main_menu_favorites, R.drawable.ic_stars_black_24dp, this::showFavorites),
+                createNavigationItem(R.string.main_menu_schedule, R.drawable.ic_list_black_24dp, this::showSchedule),
+                createNavigationItem(R.string.main_menu_map, R.drawable.ic_map_black_24dp, this::showMap)
+            )
+            .withStickyFooter(
+                (ViewGroup) createNavigationItem(R.string.main_menu_settings, R.drawable.ic_home_black_24dp, this::showSettings).generateView(this)
+            )
+            .build();
+    }
+
+    /**
+     * Create a navigation item for the drawer.
+     */
+    private PrimaryDrawerItem createNavigationItem(@StringRes int label, @DrawableRes int icon, Drawer.OnDrawerItemClickListener listener)
+    {
+        return new PrimaryDrawerItem()
+            .withName(label)
+            .withIcon(icon)
+            .withOnDrawerItemClickListener(listener)
+            .withTextColor(getResources().getColor(R.color.drawer_text_color))
+            .withSelectedTextColor(getResources().getColor(R.color.drawer_text_color))
+            .withIconColor(getResources().getColor(R.color.drawer_icon_color));
     }
 }
